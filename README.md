ec7cd44300fd9314612484b2350ec61e79644fc2
Aaditya <aaditya.singh.met16@itbhu.ac.in>
2020-04-13 02:02:09 +0530

    Added usage to README.

--- README.md
+++ README.md
@@ -16,3 +16,24 @@ pip install djangorestframework
 # Run API Server
 python manage.py runserver
 ```
+
+## Usage
+```bash
+# Users can be added by the administrator only
+# Authentication is Basic i.e. username and password.
+
+# To retreive list of movies add by user
+$ curl -X GET http://127.0.0.1:8000/movies/ -u usera:thisthisthis
+# [{"id":2,"title":"Parasite","date":"2020-01-31"}]
+
+# To search for a movie in all of the database
+$ curl -X GET http://127.0.0.1:8000/movies/\?search\=pre -u usera:thisthisthis
+# [{"id":4,"title":"Prestige","date":"2006-10-17"}]
+
+# To add a movie
+$ curl -X POST http://127.0.0.1:8000/movies/ -u usera:thisthisthis -d 'title=Maleficent&date=2014-05-30'
+# {"id":5,"title":"Maleficent","date":"2014-05-30"}
+
+# To get list of users
+$ curl -X GET http://127.0.0.1:8000/users/ -u usera:thisthisthis
+```
--- movies/views.py
+++ movies/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from rest_framework.authtoken.models import Token
 
 # Create your views here.
@@ -13,24 +13,29 @@ from .serializers import MoviesSerializer, UserSerializer
 
 
 class MovieList(generics.ListCreateAPIView):
-    # queryset = Movie.objects.all()
     serializer_class = MoviesSerializer
     permission_classes = [permissions.IsAuthenticated,
                           IsOwnerOrReadOnly]
     ordering_fields = ['title', 'date']
     ordering = ['title']
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['^title']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
     def get_queryset(self):
         user = self.request.user
+        search = self.request.query_params.get('search', None)
         sort = self.request.query_params.get('sort', None)
-        queryset = Movie.objects.filter(owner=user)
-        if sort is not None:
-            queryset = queryset.order_by(sort)
+        if search is not None:
+            queryset = Movie.objects.all()
         else:
-            queryset = queryset.order_by('title')
+            queryset = Movie.objects.filter(owner=user)
+            if sort is not None:
+                queryset = queryset.order_by(sort)
+            else:
+                queryset = queryset.order_by('title')
 
         return queryset
 
